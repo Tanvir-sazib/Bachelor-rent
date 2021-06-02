87498d87a8bacc5cdf91d80afd6ea4a767be124f
Tanvir Sazib <tansazib@gmail.com>
2021-06-02 14:37:30 +0600

    card completed

--- src/app/components/HouseCard/HouseCard.js
+++ src/app/components/HouseCard/HouseCard.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './HouseCard.css'
+import './HouseCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed, faMapMarkerAlt, faBath, faSquare } from '@fortawesome/free-solid-svg-icons';
+
 const HouseCard = ({ house }) => {
     return (
         <div className='col-lg-4 m-0 p-0 d-flex justify-content-center'>
@@ -7,9 +10,24 @@ const HouseCard = ({ house }) => {
                 <div className="card-img">
                     <img src={house.image} alt="" />
                 </div>
-                <div className="card-txt">
-                    <h5>{house.title}</h5>
-
+                <div className="card-txt p-3">
+                    <h5 className='card-title'><b>{house.title}</b></h5>
+                    <p className='text-secondary'><FontAwesomeIcon icon={faMapMarkerAlt} /> {house.address}</p>
+                    <div className="basic-info d-flex justify-content-around">
+                        <div className="bed-room">
+                            <p><FontAwesomeIcon icon={faBed} /> <b>{house.bedrooms}</b></p>
+                        </div>
+                        <div className="bath-room">
+                            <p><FontAwesomeIcon icon={faBath} /> <b>{house.bathrooms}</b></p>
+                        </div>
+                        <div className="size">
+                            <p><FontAwesomeIcon icon={faSquare} /> <b>{house.size} squre feet</b></p>
+                        </div>
+                    </div>
+                    <div className="separator"></div>
+                    <div className="price pt-2">
+                        <h5><b>{house.rent}/- BDT</b></h5>
+                    </div>
                 </div>
 
             </div>
